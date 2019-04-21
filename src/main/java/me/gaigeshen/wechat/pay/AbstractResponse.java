cb8a90ce8832cbb9cdd5b4755c8b956c04733282
gaigeshen <gaigeshen@qq.com>
2019-04-21 13:11:35 +0800

    抽象请求结果添加方法可以用来判断通信和业务结果

--- src/main/java/me/gaigeshen/wechat/pay/AbstractResponse.java
+++ src/main/java/me/gaigeshen/wechat/pay/AbstractResponse.java
@@ -9,13 +9,36 @@ import lombok.Getter;
  */
 @Getter
 public abstract class AbstractResponse implements Response {
-  private String returnCode;
-  private String returnMsg;
+  private static final String RETURN_CODE_SUCCESS = "SUCCESS";
+  private static final String RETURN_CODE_FAIL = "FAIL";
+  private static final String RESULT_CODE_SUCCESS = "SUCCESS";
+  private static final String RESULT_CODE_FAIL = "FAIL";
+
+  private String returnCode;  // 返回状态码，此字段是通信标识，非交易标识
+  private String returnMsg;   // 返回信息，当返回状态码为失败时为错误原因
+  private String resultCode;  // 业务结果
+  private String errCode;     // 错误代码
+  private String errCodeDes;  // 错误代码描述
   private String appid;
   private String mchId;
   private String nonceStr;
   private String sign;
-  private String resultCode;
-  private String errCode;
-  private String errCodeDes;
+
+  /**
+   * 返回是否通信成功
+   *
+   * @return 是否通信成功
+   */
+  public boolean isReturnSucceeded() {
+    return RETURN_CODE_SUCCESS.equals(returnCode);
+  }
+
+  /**
+   * 返回是否业务成功
+   *
+   * @return 是否业务成功
+   */
+  public boolean isResultSucceeded() {
+    return RESULT_CODE_SUCCESS.equals(resultCode);
+  }
 }
